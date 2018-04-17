fc81ebeeaacaaf3487e8c515f1496635ac90fe27
Anton Kalashnikov <kalashnikov@pythagoras.itb.pri>
2018-04-17 17:53:21 +0200

    Added the ability to add different parameters for every oscillator. Very satisfying.

--- n_coupled_oscillators.py
+++ n_coupled_oscillators.py
@@ -1,23 +1,39 @@
-# function that returns dy/dt
+# This code integrates over a system of ODEs of coupled oscillators with parameters.
+# The single oscillator is implemented as a modified Poincare oscillator (with twist - parameter, linking period and amplitude).
+
 
 from scipy.integrate import odeint
 import numpy as np
 import matplotlib.pyplot as plt
 
 
+# Complementary function for solver (oscillator_system) needed to solve multiple coupled (having terms that refer to each other) equations simultaneously.
+# In case you have a low number of equations (up to 4-6) it might be OK to just explicitely 
+# set the equations and parameters in the solver itself. However, if you want tp compute more equations, as well as set the values for every parameter separately and easily change the number of equations you have,
+# This code might help you.
+
 def oscillator(x, y, t, i, alpha, A, omega, twist, K): 
-    
+    # It takes x and y (as well as parameters) as vectors, then throws out dx/dt and dy/dt for every respective value
     x1 = x[i]
     y1 = y[i]
-    dx1dt = x1*alpha*(A-np.sqrt(x1**2 + y1**2)) - y1*(omega + twist*(A - np.sqrt(x1**2 + y1**2))) + K*(np.mean(x))
-    dy1dt = y1*alpha*(A-np.sqrt(x1**2 + y1**2)) + x1*(omega + twist*(A - np.sqrt(x1**2 + y1**2)))
+    
+    alpha1 = alpha[i]
+    A1 = A[i]
+    omega1 = omega[i]
+    twist1=twist[i]
+    K1 = K[i]
+    
+    dx1dt = x1*alpha1*(A1-np.sqrt(x1**2 + y1**2)) - y1*(omega1 + twist1*(A1 - np.sqrt(x1**2 + y1**2))) + K1*(np.mean(x))
+    dy1dt = y1*alpha1*(A1-np.sqrt(x1**2 + y1**2)) + x1*(omega1 + twist1*(A1 - np.sqrt(x1**2 + y1**2)))
 
     return dx1dt, dy1dt
 
 
+# Solver of ODEs
 def oscillator_system(state_vector, t, alpha, A, omega, twist, K):
 
-    # It receives initial conditions as a list in form [x1,y1,x2,y2,x3,y3,x4,y4,...], where x1,y1 are initial conditions for the 1st oscillator
+    # It takes initial conditions as a list in form [x1,y1,x2,y2,x3,y3,x4,y4,...], 
+    # where x1,y1 are initial conditions for the 1st oscillator
     # And converts it into array with 2 columns
     state_mat = np.array(state_vector).reshape(-1, 2)
     
@@ -38,18 +54,32 @@ def oscillator_system(state_vector, t, alpha, A, omega, twist, K):
 
 
 
-t = np.linspace(0, 500, 1000)
-params = (0.1,1,(np.pi*2)/24, 0.2, 0.05) #Turn twist up just a bit, and you'll see a very different pic
-state0 = [1,1,2,2,4,4]
+t = np.linspace(0, 1000, 1500)
 
+# PARAMETERS
+# Every parameter is set by a list, where 0-th element (e.g., omega[0]) represent 
+# the value of the parameter for the 0-th oscillator. 
+# If the values across the oscillators are the same, you may just put [value]*number_of_oscillators
+
+# Period of different oscillator varies +-1 SND (standart normal distribution) from 24h.
+# Omega is derived as (2pi/24+-1SND) 
+omeg = [(np.pi*2)/(24+i) for i in np.random.randn(10)]
+params = ([0.1]*10,[1]*10,omeg,[0.1]*10,[0.05]*10)
+state0 = [1,1]*10
+
+# Solving ODEs
 x1 = odeint(oscillator_system, state0, t, args = (params))
-
 
 n = int(len(state0) / 2)
 
+# Plotting x-coordinates of oscillators, mean and variance
 plt.figure(figsize=(20,8))
 for i in range(n):
     plt.plot(x1[:, 2*i], label = 'x coord of {}st osc'.format(i))
+plt.plot(np.mean(x1, axis=1), 'o', label = 'mean')
+#plt.plot(np.var(x1, axis=1), 'o', label = 'variance of coordinate')
+
 plt.ylim(-2,5)
 plt.legend()
 plt.show()
+
